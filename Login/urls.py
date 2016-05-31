9c9f58dc3b0e2b3b1288dc1b02410a8a314e4989
Tomasz Ścibiorek <tomasz.scibiorek@exaco.pl>
2016-05-31 18:27:29 +0200

    url reversing

--- Login/urls.py
+++ Login/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', views.login_form, name='index'),
-    url(r'^logout$', views.logout_user, name='index'),
-    url(r'^check_login$', views.check_login, name='index')
+    url(r'^logout$', views.logout_user, name='logout'),
+    url(r'^check_login$', views.check_login, name='check')
 ]
