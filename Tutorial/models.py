bc19dce32e18281c0ada4b5a599b7d200f2d5a75
Tomasz Ścibiorek <tomasz.scibiorek@exaco.pl>
2016-06-01 00:27:23 +0200

    added random value and sorting

--- Tutorial/models.py
+++ Tutorial/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=20)
+    random_value = models.IntegerField()
 
     def __unicode__(self):
-        return "This <b>objects name </b>is %s" % self.name
+        return "This <b>objects name </b>is %s and random value %d" % (self.name, self.random_value)
--- Tutorial/views.py
+++ Tutorial/views.py
@@ -1,3 +1,5 @@
+import random
+
 from Tutorial.models import Item
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
@@ -5,8 +7,12 @@ from django.shortcuts import render
 
 def index(request):
     if request.user.is_authenticated():
+        param = "?order"
         objects = Item.objects.all()
-        return render(request, "index.html", {"items": objects})
+        if "order" in request.GET:
+            objects = objects.order_by("random_value")
+            param = None
+        return render(request, "index.html", {"items": objects, "order": param})
     else:
         return HttpResponseRedirect("/error")
 
@@ -21,6 +27,7 @@ def add_item(request):
         item = Item(name="Object %d" % (objects.last().id + 1))
     else:
         item = Item(name="Object 0")
+    item.random_value = random.randint(0, 1000000)
     item.save()
     return HttpResponseRedirect("/")
 
