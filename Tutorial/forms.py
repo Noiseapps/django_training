efb5fc8ac4556fcb0a155ed5e10fe7326bddb145
Tomasz Ścibiorek <tomasz.scibiorek@exaco.pl>
2016-05-31 11:24:35 +0200

    login validation

---
+++ Tutorial/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+
+class MyLoginForm(forms.Form):
+    username = forms.CharField(max_length=25, label="Username")
+    password = forms.CharField(widget=forms.PasswordInput(), label="Password")
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if not username or len(username) == 0:
+            raise ValidationError("Username cannot be empty")
+        return username
+
+    def clean_password(self):
+        password = self.cleaned_data.get("password")
+        if not password or len(password) == 0:
+            raise ValidationError("Password cannot be empty")
+        return password
--- Tutorial/views.py
+++ Tutorial/views.py
@@ -1,7 +1,9 @@
 from Tutorial.models import Item
 from django.contrib.auth import authenticate, login, logout
+from django.core.exceptions import ValidationError
 from django.shortcuts import render
 from django.http.response import HttpResponse, HttpResponseRedirect
+from forms import MyLoginForm
 # Create your views here.
 
 
@@ -34,20 +36,31 @@ def remove_item(request, item_id):
 
 def check_login(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return HttpResponseRedirect("/")
+        form = MyLoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect("/")
+                else:
+                    form.add_error(None, ValidationError("User is deactivated"))
+            else:
+                form.add_error(None, ValidationError("Username and passwords do not match"))
+        return render(request, "login.html", {"form": form})
     return HttpResponseRedirect("/error")
 
 
 def login_form(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect("/")
-    return render(request, "login.html")
+    if request.method == "POST":
+        return check_login(request)
+    else:
+        form = MyLoginForm()
+        return render(request, "login.html", {"form": form})
 
 
 def logout_user(request):
